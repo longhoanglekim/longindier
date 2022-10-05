117ebde22509426c1fd625c0f5b8f90bd566b638
longhoanglekim <115090953+longhoanglekim@users.noreply.github.com>
2022-10-05 21:17:19 +0700

    Add files via upload

---
+++ while.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+ main() {
+ 	/*int i =0;
+ 	while ( i++ < 10) // there won't be 0 and end at 10
+ 	cout << i <<endl ;*/
+ 	/* const int sizeofarray =10 ;
+ 	int i = 0;
+ 	int array[sizeofarray];
+ 	while (i <  sizeofarray){
+ 		array[i] = 10 * i;
+ 		cout << array[i] <<endl;
+		i++; 
+ 	}*/
+ 	int i;
+	 do 
+	 {
+	 	cout<<"lala"<<endl;
+	 }while(i);
+	 
+ 	
+	return 0;
+}
